65ad9c5db4f05692dd1c4b71337305a91625e1c4
fyugetty <fyugetty@mail.ru>
2026-04-23 18:39:45 +0300

    add sort and print

---
+++ cost.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include "cost.h"
+
+void print_car(struct Car arr[], int syze) {
+	for (int i = 0; i < syze; i++) {
+		printf("%d.%d.%d %s %s %d", arr[i].date.day, arr[i].date.month, arr[i].date.year, arr[i].manager, arr[i].marka, arr[i].cost);
+	}
+}
+
+int camper(struct Car a[], struct Car b[]) {
+	return strcmp(a, b);
+}
+
+void sorted_viborom(struct Car arr[], int syze) {
+	int min = 0;
+	for (int i = 0; i < syze; i++) {
+		min = i;
+		for (int j = i; j < syze; j--) {
+			if (camper(j, min) > 0) {
+				min = j;
+			}
+		}
+		struct Car tmp = arr[i];
+		arr[i] = arr[min];
+		arr[min] = tmp;
+	}
+}
+
--- cost.h
+++ cost.h
@@ -1,1 +1,15 @@
 #pragma once
+#include <stdio.h>
+#include <string.h>
+
+struct Date {
+	int day;
+	int month;
+	int year;
+};
+struct Car {
+	struct Date date;
+	char manager[100];
+	char marka[40];
+	int cost;
+};
